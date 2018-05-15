cbbb4192360e68b9b3c4cdbaa3f40d4148d5d84a
imandin408_home <imandin408@gmail.com>
2018-05-14 20:14:44 -0400

    home 5/14/18

--- app/src/main/java/com/example/a10013461/attendanceapp/ReadTextFragment.java
+++ app/src/main/java/com/example/a10013461/attendanceapp/ReadTextFragment.java
@@ -6,11 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ReadTextFragment extends Fragment {
 
     ListView listView;
+
+    ClassElement classElement = getArguments().getParcelable("classList");
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +26,10 @@ public class ReadTextFragment extends Fragment {
         View fragmentView = inflater.inflate(R.layout.fragment_read_text_fragment,container,false);
 
         listView = (ListView) fragmentView.findViewById(R.id.listOfNames);
+        list = classElement.getPeople();
+        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,list);
+        adapter.notifyDataSetChanged();
+
 
         return fragmentView;
     }
